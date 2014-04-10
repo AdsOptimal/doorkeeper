389c380bb0fa3b47b0cc11c3fbf0925b8b618585
jasukkas <jasukkas.th@gmail.com>
2014-04-10 11:25:19 +0700

    Fix bug

--- lib/doorkeeper/helpers/filter.rb
+++ lib/doorkeeper/helpers/filter.rb
@@ -14,7 +14,7 @@ module Doorkeeper
               headers['Access-Control-Allow-Origin'] = '*'
               headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
               headers['Access-Control-Max-Age'] = '1000'
-              headers['Access-Control-Allow-Headers'] = '*, origin, accept'
+              headers['Access-Control-Allow-Headers'] = '*, accept, authorization'
               
               render_options = doorkeeper_unauthorized_render_options
 
